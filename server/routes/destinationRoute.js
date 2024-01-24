3092028a629674fc10a73243397e646f4311fc26
Calvin <calvinng1993@gmail.com>
2024-01-24 13:53:22 +0000

    Add end points for destination and sites in Routes folder

---
+++ server/routes/destinationRoute.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const knex = require("knex")(require("../knexfile"));
+
+router.get("/", async (_req, res) => {
+  try {
+    const destination = await knex("destination");
+    res.status(200).json(destination);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: "Failed to grab destination." });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const sites = await knex("site").where({ destination_id: id });
+    res.status(200).json(sites);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: "Failed to grab sites." });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const SERVER_PORT = process.env.PORT;
+const destinationRoute = require("./routes/destinationRoute");
+const PORT = process.env.PORT || 8080;
 const SERVER_API_URL = process.env.API_URL;
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/destination", destinationRoute);
+
 app.listen(
-  SERVER_PORT,
-  console.log(`Server has been started at ${SERVER_API_URL}:${SERVER_PORT}`)
+  PORT,
+  console.log(`Server has been started at ${SERVER_API_URL}:${PORT}`)
 );
