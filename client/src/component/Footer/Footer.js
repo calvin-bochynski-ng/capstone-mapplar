15f3c24cc67f8fa7a13874e804820c3ea8745674
Calvin <calvinng1993@gmail.com>
2024-01-25 14:57:32 +0000

    Add Routers and files for Header and Footer

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,5 @@
+import "./Footer.scss";
+const Footer = () => {
+  return <div>Footer</div>;
+};
+export default Footer;
---
+++ client/src/component/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.scss";
+const Header = () => {
+  return (
+    <header>
+      <h1>This is the header</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit,
+        totam cumque consequuntur, repudiandae quaerat praesentium obcaecati eum
+        sequi, est necessitatibus commodi non deleniti. Magni, ducimus impedit.
+        At porro quo inventore.
+      </p>
+    </header>
+  );
+};
+export default Header;
